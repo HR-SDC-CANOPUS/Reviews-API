fbd9248278540e103b1858432f51f022711c6674
Michael Doudy <michael.doudy@gmail.com>
2020-07-14 15:57:47 -0400

    Add basic express server config

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.use('/', (req, res) => {
+  res.send('HELLO WORLD')
+});
+
+app.listen(port, () => console.log(`Listening at http://localhost:${port}`))
