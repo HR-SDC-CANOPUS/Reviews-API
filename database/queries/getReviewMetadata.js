fef2bad4ca463cd89533da9cff697fc1f3d0a0b8
Michael Doudy <michael.doudy@gmail.com>
2020-07-24 00:32:15 -0400

    Add getMetaData query logic

--- database/queries/getReviewMetadata.js
+++ database/queries/getReviewMetadata.js
@@ -1,25 +1,74 @@
 const client = require('../index');
 
 module.exports = (productId) => {
-  const query =
-      ` SELECT t.counts FROM
-      (SELECT json_object_agg(r.rating, (SELECT count(rating) FROM "reviews" WHERE "product_id" = ${productId} AND rating = r.rating)) AS counts
-        FROM "reviews" AS r
-        WHERE "product_id" = ${productId}
-        GROUP BY rating) AS t
-          ;`
+  const query = `
+      select rMain.product_id,
+        (
+        select jsonb_agg(outerC) from
+          (
+          SELECT json_object_agg(r2.rating,
+            (
+            SELECT count(r1.rating)
+            FROM reviews r1
+            WHERE r1.product_id = rMain.product_id AND r1.rating = r2.rating
+            )
+          ) AS counts
+        FROM reviews r2
+        WHERE r2.product_id = rMain.product_id
+        GROUP BY r2.rating) as outerC) as ratings,
+        (
+        select jsonb_agg(outerRecommendCounts) from
+          (
+          SELECT json_object_agg(r4.recommend,
+            (
+            SELECT count(r3.recommend)
+            FROM reviews r3
+            WHERE r3.product_id = rMain.product_id AND r3.recommend = r4.recommend
+            )
+          ) AS recommendCounts
+        FROM reviews r4
+        WHERE r4.product_id = rMain.product_id
+        GROUP BY r4.recommend) as outerRecommendCounts
+        ) as recommended,
+        (
+        select array_to_json(array_agg(characteristicGroup)) from
+          (
+          select c.name, c.id, avg(cr.value) as value
+          from "characteristics" c
+          inner join characteristics_reviews cr
+          on c.id = cr.characteristics_id
+          where c.product_id = rMain.product_id
+          group by c.id
+          ) characteristicGroup
+        ) as characteristics
+      from reviews rMain
+      where rMain.product_id = ${productId}
+      group by rMain.product_id
+      ;`;
 
-      // `
-      // (SELECT rating FROM "reviews" WHERE "product_id" = ${productId} GROUP BY rating)
-      // (SELECT count(rating) FROM "reviews" WHERE "product_id" = ${productId} GROUP BY rating)
-      // `
+  const metadataTransformer = (data) => {
+    let transformedRatings = {},
+      transformedRecommended = {},
+      transformedCharacteristics = {};
+    data = data.rows[0];
 
-      // ` SELECT t.counts FROM
-      // (SELECT json_object_agg(r.rating, (SELECT count(rating) FROM "reviews" WHERE "product_id" = ${productId} AND rating = r.rating)) AS counts
-      //   FROM "reviews" AS r
-      //   WHERE "product_id" = ${productId}
-      //   GROUP BY rating) AS t
-      //     ;`
+    data.ratings.forEach((row) => {
+      transformedRatings = { ...transformedRatings, ...row.counts };
+    });
+    data.ratings = transformedRatings;
 
-  return client.query(query);
-}
+    data.recommended.forEach((row) => {
+      transformedRecommended = { ...transformedRecommended, ...row.recommendcounts };
+    });
+    data.recommended = transformedRecommended;
+
+    data.characteristics.forEach((row) => {
+      transformedCharacteristics = { ...transformedCharacteristics, [row.name]: { id: row.id, value: row.value } };
+    });
+    data.characteristics = transformedCharacteristics;
+
+    return data;
+  };
+
+  return client.query(query).then((data) => metadataTransformer(data));
+};
--- server/controllers.js
+++ server/controllers.js
@@ -1,75 +1,76 @@
 const models = require('./models');
 
 module.exports = {
-
   getReviews: (req, res) => {
     const productId = req.params.product_id;
     const page = req.query.page || 1;
     const count = req.query.count || 5;
     const sort = req.query.sort || 'newest';
 
-    models.dbGetReviews({ productId, page, count, sort })
-    .then((data) => {
-      res.send({product: productId, page, count, results: data.rows});
-    })
-    .catch((err) => {
-      console.log('DB ERROR: ', err);
-      res.sendStatus(404);
-    })
+    models
+      .dbGetReviews({ productId, page, count, sort })
+      .then((data) => {
+        res.send({ product: productId, page, count, results: data.rows });
+      })
+      .catch((err) => {
+        console.log('DB ERROR: ', err);
+        res.sendStatus(404);
+      });
   },
 
   getReviewMetadata: (req, res) => {
     const productId = req.params.product_id;
 
-    models.dbGetReviewMetadata(productId)
-    .then((data) => {
-      res.send({product: productId, results: data.rows});
-    })
-    .catch((err) => {
-      console.log('DB ERROR: ', err);
-      res.sendStatus(404);
-    })
-
+    models
+      .dbGetReviewMetadata(productId)
+      .then((data) => {
+        res.send(data);
+      })
+      .catch((err) => {
+        console.log('DB ERROR: ', err);
+        res.sendStatus(404);
+      });
   },
 
   addReview: (req, res) => {
     const params = { ...req.body, productId: req.params.product_id };
 
-    models.dbAddReview(params)
-    .then((data) => {
-      console.log(data);
-      res.sendStatus(200);
-    })
-    .catch((err) => {
-      console.log('DB ERROR: ', err);
-      res.sendStatus(404);
-    })
+    models
+      .dbAddReview(params)
+      .then((data) => {
+        res.sendStatus(200);
+      })
+      .catch((err) => {
+        console.log('DB ERROR: ', err);
+        res.sendStatus(404);
+      });
   },
 
   markReviewHelpful: (req, res) => {
     const reviewId = req.params.review_id;
 
-    models.dbMarkReviewHelpful(reviewId)
-    .then((data) => {
-      res.sendStatus(200);
-    })
-    .catch((err) => {
-      console.log('DB ERROR: ', err);
-      res.sendStatus(404);
-    })
+    models
+      .dbMarkReviewHelpful(reviewId)
+      .then((data) => {
+        res.sendStatus(200);
+      })
+      .catch((err) => {
+        console.log('DB ERROR: ', err);
+        res.sendStatus(404);
+      });
   },
 
   markReviewReported: (req, res) => {
     const reviewId = req.params.review_id;
 
-    models.dbMarkReviewReported(reviewId)
-    .then((data) => {
-      res.sendStatus(200);
-    })
-    .catch((err) => {
-      console.log('DB ERROR: ', err);
-      res.sendStatus(404);
-    })
-  }
-
-}
+    models
+      .dbMarkReviewReported(reviewId)
+      .then((data) => {
+        res.sendStatus(200);
+      })
+      .catch((err) => {
+        console.log('DB ERROR: ', err);
+        res.sendStatus(404);
+      });
+  },
+};
